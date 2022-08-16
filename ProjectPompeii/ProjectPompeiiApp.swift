dd226715e561c5cdec335831c99c2ce2780a71be
Alex A. Rocha <lex99r@gmail.com>
2022-08-16 08:22:36 -0400

    Primeiro commit

---
+++ ProjectPompeii/ProjectPompeiiApp.swift
@@ -0,0 +1,20 @@
+//
+//  ProjectPompeiiApp.swift
+//  ProjectPompeii
+//
+//  Created by Alex A. Rocha on 16/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ProjectPompeiiApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
