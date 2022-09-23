5a602c4f711bf8b2cd24846457fbc9693b1e0020
Kiara Medeiros <kiaramedeiros2001@gmail.com>
2022-09-23 10:19:16 -0400

    Ajuste alertas

--- ProjectPompeii/Views/CustomAlertView.swift
+++ ProjectPompeii/Views/CustomAlertView.swift
@@ -7,6 +7,55 @@
 
 import SwiftUI
 
+struct CustomAlertTutorial: View {
+    
+    @Binding var shown: Bool
+    
+    var body: some View {
+        VStack {
+            Text("How to play?")
+                .font(.custom("Rubik-SemiBold", size: 17))
+                .frame(maxWidth: 106, maxHeight: 20)
+                .minimumScaleFactor(0.1)
+                .fixedSize(horizontal: true, vertical: false)
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color("TertiaryColor-1"))
+                .padding([.horizontal], 118)
+                .padding([.top], 16)
+
+            Text("Draw and confirm as many drawings as possible in 30 seconds!")
+                .font(.custom("Rubik-Regular", size: 13))
+                .frame(maxWidth: 214, maxHeight: 32)
+                .minimumScaleFactor(0.1)
+                .fixedSize(horizontal: true, vertical: false)
+                .foregroundColor(Color("TertiaryColor-1"))
+                .multilineTextAlignment(.center)
+                .padding([.horizontal], 28)
+            
+            Divider()
+            
+            Button {
+                shown.toggle()
+            } label: {
+                Text("Ok")
+                    .font(.custom("Rubik-SemiBold", size: 17))
+                    .frame(maxWidth: 22, maxHeight: 20)
+                    .minimumScaleFactor(0.1)
+                    .fixedSize(horizontal: false, vertical: true)
+                    .foregroundColor(Color("TertiaryColor-1"))
+                    .multilineTextAlignment(.center)
+                    .padding([.horizontal], 124)
+                    .padding([.vertical], 11)
+            }
+        }
+        .frame(width: UIScreen.main.bounds.width-120, height: 136)
+        .background(Color(UIColor(red: 1.00, green: 0.98, blue: 0.51, alpha: 1.00)))
+        .overlay(RoundedRectangle(cornerRadius: 15).strokeBorder(lineWidth: 3).foregroundColor(Color(UIColor(red: 0.14, green: 0.11, blue: 0.23, alpha: 1.00))))
+        .cornerRadius(15)
+        .clipped()
+    }
+}
+
 struct CustomAlertOops: View {
     
     @Binding var shown: Bool
@@ -16,22 +65,38 @@ struct CustomAlertOops: View {
         VStack {
             Text("Oops! :(")
                 .font(.custom("Rubik-SemiBold", size: 17))
-                .padding([.vertical], 12)
-                .foregroundColor(Color("TertiaryColor-1"))
-            Text("That's not the instructed drawing, try again, please!")
+                .frame(maxWidth: 62, maxHeight: 20)
+                .minimumScaleFactor(0.1)
                 .fixedSize(horizontal: false, vertical: true)
-                .font(.custom("Rubik-Regular", size: 13))
                 .foregroundColor(Color("TertiaryColor-1"))
                 .multilineTextAlignment(.center)
-                .padding([.horizontal], 32)
+                .padding([.top], 16)
+                .padding([.horizontal], 164)
+
+            Text("That's not the instructed drawing, try again, please!")
+                .font(.custom("Rubik-Regular", size: 13))
+                .frame(maxWidth: 235, maxHeight: 32)
+                .minimumScaleFactor(0.1)
+                .fixedSize(horizontal: false, vertical: true)
+                .foregroundColor(Color("TertiaryColor-1"))
+                .multilineTextAlignment(.center)
+                .padding([.horizontal], 28)
+            
             Divider()
+            
             Button {
                 shown.toggle()
                 isShowingAlert = false
             } label: {
                 Text("Ok")
                     .font(.custom("Rubik-SemiBold", size: 17))
+                    .frame(maxWidth: 22, maxHeight: 20)
+                    .minimumScaleFactor(0.1)
+                    .fixedSize(horizontal: false, vertical: true)
                     .foregroundColor(Color("TertiaryColor-1"))
+                    .multilineTextAlignment(.center)
+                    .padding([.horizontal], 124)
+                    .padding([.vertical], 11)
             }
         }
         .frame(width: UIScreen.main.bounds.width-120, height: 136)
@@ -52,10 +117,14 @@ struct CustomAlertTimesUp: View {
         VStack {
             Text("Time's up!")
                 .font(.custom("Rubik-SemiBold", size: 17))
+                .frame(maxWidth: 106, maxHeight: 20)
+                .minimumScaleFactor(0.1)
                 .padding([.vertical], 12)
                 .foregroundColor(Color("TertiaryColor-1"))
             Text("Your scribbliuns have been successfully counted!")
                 .font(.custom("Rubik-Regular", size: 13))
+                .frame(maxWidth: 235, maxHeight: 32)
+                .minimumScaleFactor(0.1)
                 .foregroundColor(Color("TertiaryColor-1"))
                 .multilineTextAlignment(.center)
                 .padding([.horizontal], 32)
@@ -70,39 +139,13 @@ struct CustomAlertTimesUp: View {
             } label: {
                 Text("Ok")
                     .font(.custom("Rubik-SemiBold", size: 17))
+                    .frame(maxWidth: 22, maxHeight: 20)
+                    .minimumScaleFactor(0.1)
+                    .fixedSize(horizontal: false, vertical: true)
                     .foregroundColor(Color("TertiaryColor-1"))
-            }
-        }
-        .frame(width: UIScreen.main.bounds.width-120, height: 136)
-        .background(Color(UIColor(red: 1.00, green: 0.98, blue: 0.51, alpha: 1.00)))
-        .overlay(RoundedRectangle(cornerRadius: 15).strokeBorder(lineWidth: 3).foregroundColor(Color(UIColor(red: 0.14, green: 0.11, blue: 0.23, alpha: 1.00))))
-        .cornerRadius(15)
-        .clipped()
-    }
-}
-
-struct CustomAlertBluetooth: View {
-    
-    @Binding var shown: Bool
-    
-    var body: some View {
-        VStack {
-            Text("NOOOOOO...")
-                .font(.custom("Rubik-SemiBold", size: 17))
-                .padding([.vertical], 12)
-                .foregroundColor(Color("TertiaryColor-1"))
-            Text("Bluetooth is turned off, to join a game you must turn it on!")
-                .font(.custom("Rubik-Regular", size: 13))
-                .foregroundColor(Color("TertiaryColor-1"))
-                .multilineTextAlignment(.center)
-                .padding([.horizontal], 32)
-            Divider()
-            Button {
-                shown.toggle()
-            } label: {
-                Text("Ok")
-                    .font(.custom("Rubik-SemiBold", size: 17))
-                    .foregroundColor(Color("TertiaryColor-1"))
+                    .multilineTextAlignment(.center)
+                    .padding([.horizontal], 124)
+                    .padding([.vertical], 11)
             }
         }
         .frame(width: UIScreen.main.bounds.width-120, height: 136)
@@ -122,10 +165,14 @@ struct CustomAlertEmpty: View {
         VStack {
             Text("Oops!")
                 .font(.custom("Rubik-SemiBold", size: 17))
+                .frame(maxWidth: 106, maxHeight: 20)
+                .minimumScaleFactor(0.1)
                 .padding([.vertical], 12)
                 .foregroundColor(Color("TertiaryColor-1"))
             Text("You can't proceed with an empty drawing!")
                 .font(.custom("Rubik-Regular", size: 13))
+                .frame(maxWidth: 235, maxHeight: 32)
+                .minimumScaleFactor(0.1)
                 .foregroundColor(Color("TertiaryColor-1"))
                 .multilineTextAlignment(.center)
                 .padding([.horizontal], 32)
@@ -136,39 +183,13 @@ struct CustomAlertEmpty: View {
             } label: {
                 Text("Ok")
                     .font(.custom("Rubik-SemiBold", size: 17))
+                    .frame(maxWidth: 22, maxHeight: 20)
+                    .minimumScaleFactor(0.1)
+                    .fixedSize(horizontal: false, vertical: true)
                     .foregroundColor(Color("TertiaryColor-1"))
-            }
-        }
-        .frame(width: UIScreen.main.bounds.width-120, height: 136)
-        .background(Color(UIColor(red: 1.00, green: 0.98, blue: 0.51, alpha: 1.00)))
-        .overlay(RoundedRectangle(cornerRadius: 15).strokeBorder(lineWidth: 3).foregroundColor(Color(UIColor(red: 0.14, green: 0.11, blue: 0.23, alpha: 1.00))))
-        .cornerRadius(15)
-        .clipped()
-    }
-}
-
-struct CustomAlertTutorial: View {
-    
-    @Binding var shown: Bool
-    
-    var body: some View {
-        VStack {
-            Text("How to play?")
-                .font(.custom("Rubik-SemiBold", size: 17))
-                .padding([.vertical], 12)
-                .foregroundColor(Color("TertiaryColor-1"))
-            Text("Draw as many correct scribbliuns as you can in 30 seconds!")
-                .font(.custom("Rubik-Regular", size: 13))
-                .foregroundColor(Color("TertiaryColor-1"))
-                .multilineTextAlignment(.center)
-                .padding([.horizontal], 32)
-            Divider()
-            Button {
-                shown.toggle()
-            } label: {
-                Text("Ok")
-                    .font(.custom("Rubik-SemiBold", size: 17))
-                    .foregroundColor(Color("TertiaryColor-1"))
+                    .multilineTextAlignment(.center)
+                    .padding([.horizontal], 124)
+                    .padding([.vertical], 11)
             }
         }
         .frame(width: UIScreen.main.bounds.width-120, height: 136)
@@ -189,10 +210,14 @@ struct CustomLeaveDrawView: View {
         VStack {
             Text("Please wait!")
                 .font(.custom("Rubik-SemiBold", size: 17))
+                .frame(maxWidth: 99, maxHeight: 20)
+                .minimumScaleFactor(0.1)
                 .padding([.vertical], 12)
                 .foregroundColor(Color("TertiaryColor-1"))
             Text("If you leave now you will lose your current progress!")
                 .font(.custom("Rubik-Regular", size: 13))
+                .frame(maxWidth: 235, maxHeight: 32)
+                .minimumScaleFactor(0.1)
                 .foregroundColor(Color("TertiaryColor-1"))
                 .multilineTextAlignment(.center)
                 .padding([.horizontal], 32)
@@ -249,10 +274,14 @@ struct CustomPaused: View {
         VStack {
             Text("Game paused")
                 .font(.custom("Rubik-SemiBold", size: 17))
+                .frame(maxWidth: 112, maxHeight: 20)
+                .minimumScaleFactor(0.1)
                 .padding([.vertical], 12)
                 .foregroundColor(Color("TertiaryColor-1"))
             Text("You paused the game")
                 .font(.custom("Rubik-Regular", size: 13))
+                .frame(maxWidth: 129, maxHeight: 16)
+                .minimumScaleFactor(0.1)
                 .foregroundColor(Color("TertiaryColor-1"))
                 .multilineTextAlignment(.center)
                 .padding([.horizontal], 32)
@@ -280,6 +309,49 @@ struct CustomPaused: View {
     }
 }
 
+//struct CustomAlertBluetooth: View {
+//
+//    @Binding var shown: Bool
+//
+//    var body: some View {
+//        VStack {
+//            Text("NOOOOOO...")
+//                .font(.custom("Rubik-SemiBold", size: 17))
+//                .frame(maxWidth: 106, maxHeight: 20)
+//                .minimumScaleFactor(0.1)
+//                .padding([.vertical], 12)
+//                .foregroundColor(Color("TertiaryColor-1"))
+//            Text("Bluetooth is turned off, to join a game you must turn it on!")
+//                .font(.custom("Rubik-Regular", size: 13))
+//                .frame(maxWidth: 214, maxHeight: 32)
+//                .minimumScaleFactor(0.1)
+//                .foregroundColor(Color("TertiaryColor-1"))
+//                .multilineTextAlignment(.center)
+//                .padding([.horizontal], 32)
+//            Divider()
+//            Button {
+//                shown.toggle()
+//            } label: {
+//                Text("Ok")
+//                    .font(.custom("Rubik-SemiBold", size: 17))
+//                    .frame(maxWidth: 22, maxHeight: 20)
+//                    .minimumScaleFactor(0.1)
+//                    .fixedSize(horizontal: false, vertical: true)
+//                    .foregroundColor(Color("TertiaryColor-1"))
+//                    .multilineTextAlignment(.center)
+//                    .padding([.horizontal], 124)
+//                    .padding([.vertical], 11)
+//            }
+//        }
+//        .frame(width: UIScreen.main.bounds.width-120, height: 136)
+//        .background(Color(UIColor(red: 1.00, green: 0.98, blue: 0.51, alpha: 1.00)))
+//        .overlay(RoundedRectangle(cornerRadius: 15).strokeBorder(lineWidth: 3).foregroundColor(Color(UIColor(red: 0.14, green: 0.11, blue: 0.23, alpha: 1.00))))
+//        .cornerRadius(15)
+//        .clipped()
+//    }
+//}
+
+
 
 struct CustomAlertView_Previews: PreviewProvider {
     static var previews: some View {
@@ -289,8 +361,8 @@ struct CustomAlertView_Previews: PreviewProvider {
             navigationBond: .constant(NavigationBond(destination: .cleo)),
             isShowingAlert: .constant(false)
         )
-        CustomAlertBluetooth(shown: .constant(false))
         CustomAlertEmpty(shown: .constant(false), isShowingAlert: .constant(false))
         CustomAlertTutorial(shown: .constant(false))
+        //CustomAlertBluetooth(shown: .constant(false))
     }
 }
