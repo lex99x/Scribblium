0bbc5031357de3a61a313257099c8af429d112ba
Kiara Medeiros <kiaramedeiros2001@gmail.com>
2022-09-28 09:53:02 -0400

    Adicionando efeitos sonoros e hápticos na DrawView

---
+++ ProjectPompeii/Models/SoundManager.swift
@@ -0,0 +1,36 @@
+//
+//  SoundManager.swift
+//  ProjectPompeii
+//
+//  Created by Ana Kiara Medeiros Braga on 28/09/22.
+//
+
+import Foundation
+import SwiftUI
+import AVKit
+
+class SoundManager {
+    
+    static let instance = SoundManager()
+    
+    var player: AVAudioPlayer?
+    
+    enum sounds: String {
+        case correct1
+        case correct2
+        case correct3
+    }
+    
+    func playSound(sound: sounds) {
+        guard let url = Bundle.main.url(forResource: sound.rawValue, withExtension: ".mp3") else { return }
+        
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()
+        }
+        
+        catch let error {
+            print("Error playing sound. \(error.localizedDescription)")
+        }
+    }
+}
